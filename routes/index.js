c19f40bf04300df0bf7b44f63684e5ec5907fd45
Asli Sevil Ozcan <aslisevil@hotmail.com>
2022-10-17 03:22:25 +0100

    Added update user functionality.

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,49 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const { requiresAuth } = require('express-openid-connect');
 const { urlencoded } = require('body-parser');
+const postgres = require('pg');
 const router = express.Router();
+require('dotenv').config();
+
+const conString = process.env.POSTGRE_URL;
 
 let encodeUrl = bodyParser.urlencoded({ extended: false })
 
 router.get('/', (req, res) => {
     console.log(req.oidc.isAuthenticated());
     res.render('index', { 
-        title: "Express",
+        title: "Home Page",
         isAuthenticated: req.oidc.isAuthenticated(),
         user: req.oidc.user,
     });
 });
 
 router.get('/profile', requiresAuth(), (req, res) => {
-  //res.send(JSON.stringify(req.oidc.user));
-  res.render('profile', { 
-        title: "Profile Page",
-        isAuthenticated: req.oidc.isAuthenticated(),
-        user: req.oidc.user,
+    var pool = new postgres.Client(conString);
+    
+    pool.connect( function (err, client, done) {
+        if(err) return cancelIdleCallback(err);
+
+        const query = 'SELECT * FROM users WHERE email=$1';
+        
+        client.query(query, [req.oidc.user.email], function(err, result){
+            //done();
+            if(err) return console.error('error running query', err);
+
+            res.render('profile', { 
+                title: "Profile Page",
+                isAuthenticated: req.oidc.isAuthenticated(),
+                user: req.oidc.user,
+                results: result.rows[0],
     });
+            console.log(result.rows[0]);
+            client.end();
+        });
+        
+    });
+    
+    
+  
 });
 
 router.get('/update', requiresAuth(), (req, res) => {
@@ -34,8 +57,22 @@ router.get('/update', requiresAuth(), (req, res) => {
 });
 
 router.post('/', encodeUrl, (req, res) => {
-    console.log('Got body: ', req.body)
-    res.sendStatus(200);
+    var pool = new postgres.Client(conString);
+    
+    console.log(req.oidc.user)
+    pool.connect(function (err, client, done) {
+        if(err) console.log(err);
+
+        const query = 'UPDATE users SET firstname=$1, lastname=$2 WHERE email=$3';
+        client.query(query, [req.body.firstname, req.body.lastname, req.oidc.user.email], function(err, result){
+            //done();
+            if(err) return console.error('error running query', err);
+            
+            client.end();
+        });
+        
+    });
+    res.redirect('/');
 });
 
 module.exports = router;
